dd4361bff5b8f8220f4f4ed8630b4412780462ac
MuriloMarquesSantos <murilommms@gmail.com>
2020-07-11 18:25:17 -0300

    Delivering challenged completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,57 @@ interface Balance {
   total: number;
 }
 
+interface TransactionsResponse {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
+  private balance: Balance;
+
   private transactions: Transaction[];
 
   constructor() {
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all() {
+    return {
+      transactions: this.transactions,
+      balance: this.balance,
+    };
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public create(transaction: Transaction): Transaction {
+    this.transactions.push(transaction);
+    if (transaction.type === 'outcome') {
+      this.performWithdraw(transaction.value);
+    } else {
+      this.performDeposit(transaction.value);
+    }
+    return transaction;
   }
 
-  public create(): Transaction {
-    // TODO
+  private performWithdraw(transactionValue: number): void {
+    if (transactionValue > this.balance.total) {
+      throw new Error('Insufficient Balance');
+    }
+    this.balance.outcome += Math.abs(transactionValue);
+    this.setTotalBalance();
+  }
+
+  private performDeposit(transactionValue: number): void {
+    this.balance.income += Math.abs(transactionValue);
+    this.setTotalBalance();
+  }
+
+  private setTotalBalance(): void {
+    this.balance.total = this.balance.income - this.balance.outcome;
   }
 }
 
---
+++ src/services/GetTransactionsService.ts
@@ -0,0 +1,27 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface TransactionsResponse {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+class GetTransactionsService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute(): TransactionsResponse {
+    return this.transactionsRepository.all();
+  }
+}
+
+export default GetTransactionsService;
